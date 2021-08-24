c3bf81663c0a0040b125558ee43547dc71648a97
asvattha <singhd1@sfasu.edu>
2021-08-24 16:02:15 -0500

    First OOP program (Lecture 1)
    It covers the following:
    1. How to write a class
    2. How to create an object of class type
    3. How to write default constructor
    4. What are public and private access specifiers
    5. How to access the private member variables outside the class (getter and setter methods)

--- OOP_Basics/Program.cs
+++ OOP_Basics/Program.cs
@@ -9,6 +9,7 @@ namespace OOP_Basics
            Dog myDog = new Dog(); // myDog is initialized with name set as Tommy and age as 1. 
             myDog.Name = "Rocky"; // Overwriting the name Tommy with Rocky. If you comment this out then it will print Tommy. 
             Console.WriteLine(myDog.Name);
+
         }
     }
 }
