c2e9c89e59b94b700fe9504a2117f2df2f377435
asvattha <singhd1@sfasu.edu>
2021-08-24 16:00:09 -0500

    Add project files.

---
+++ OOP_Basics/Dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Basics
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; }
+
+        public Dog()
+        {
+            Name = "Tommy";
+            Age = 1;
+        }
+
+        private void bark()
+        {
+            Console.WriteLine("Woof Woof");
+        }
+    }
+}
---
+++ OOP_Basics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Dog myDog = new Dog(); // myDog is initialized with name set as Tommy and age as 1. 
+            myDog.Name = "Rocky"; // Overwriting the name Tommy with Rocky. If you comment this out then it will print Tommy. 
+            Console.WriteLine(myDog.Name);
+        }
+    }
+}
