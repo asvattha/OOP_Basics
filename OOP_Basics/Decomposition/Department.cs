73ed58bba833e8876373d63d3a792d9a4506ab07
asvattha <singhd1@sfasu.edu>
2021-09-01 16:41:04 -0500

    Decomposition-Revised

--- OOP_Basics/Decomposition/Department.cs
+++ OOP_Basics/Decomposition/Department.cs
@@ -14,7 +14,7 @@ namespace Decomposition
             teachers = t; // usually the smaller part are initialized by the argument passed to the constructor or any method to the bigger class
             /* Here the teachers is initialized with the t passed to the Department. 
              * Also, it is important to note that, the List t or any object is passed as reference and not by value. Therefore, if you make any change
-             * to t attributes, then every teacher object attributes will be changed.
+             * to t attributes, then every teacher object that refers to t will be changed.
              */
             this.name = name;
         }
